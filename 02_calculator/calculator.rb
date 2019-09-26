cbfdeebcbfa93443af22f04a00f7bd300024616a
James DeLayo <james.delayo@gmail.com>
2019-09-26 17:57:33 -0400

    Completes first three exercises

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,15 @@
-#write your code here
+def add(a,b)
+	a + b
+end
+
+def subtract(a,b)
+	a - b
+end
+
+def sum(input)
+	input.sum
+end
+
+def multiply(input)
+	input.inject(:*)
+end
--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -77,23 +77,12 @@ end
 # once the above tests pass,
 # write tests and code for the following:
 
-describe "#multiply" do
+describe "multiply" do
+  it "multiplies two numbers" do
+    expect(multiply([2,4])).to eq(8)
+  end
 
-  it "multiplies two numbers"
-
-  it "multiplies several numbers"
-  
-end
-
-describe "#power" do
-  it "raises one number to the power of another number"
-end
-
-# http://en.wikipedia.org/wiki/Factorial
-describe "#factorial" do
-  it "computes the factorial of 0"
-  it "computes the factorial of 1"
-  it "computes the factorial of 2"
-  it "computes the factorial of 5"
-  it "computes the factorial of 10"
+  it "multiplies several numbers" do
+    expect(multiply([2,4,6])).to eq(48)
+  end 
 end
